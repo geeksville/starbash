a7a30fb1d4a0d41fae18fa692f2314d94cdc753e
Kevin Hester <kevinh@geeksville.com>
2025-10-25 11:31:19 +0000

    better dockerfile init

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -8,18 +8,6 @@ echo "Fixing permissions"
 
 echo "source .devcontainer/on-shell-start.sh" >> ~/.bashrc
 
-echo "installing siril flatpak..."
-
-# Install Siril (as non-root user)
-# Note: this must be done here, if done in Dockerfile it doesn't work - bcause dbus is not yet running
-# https://github.com/flatpak/flatpak/issues/5076
-
-flatpak --user remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install --user -y flathub org.siril.Siril
-
-# Let siril see /tmp
-flatpak --user override --filesystem=/tmp org.siril.Siril
-
 echo "setup poetry..."
 
 # Setup poetry build env
