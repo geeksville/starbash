a405e3ccc82316fa9bb0ef10691311c64dc0a32a
Kevin Hester <kevinh@geeksville.com>
2025-10-25 11:37:15 +0000

    update license

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-export USER=`whoami`
-
-# the devcontainer mount of vscode/.local/share implicity makes the owner root (which is bad)
-echo "Fixing permissions"
-
 echo "source .devcontainer/on-shell-start.sh" >> ~/.bashrc
-
-echo "setup poetry..."
 
 # Setup poetry build env
 poetry completions bash >> ~/.bash_completion
