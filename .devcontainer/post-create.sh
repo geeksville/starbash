87e1248c2216cf09ad3599b99a04a905ec5763a3
Kevin Hester <kevinh@geeksville.com>
2025-10-31 00:11:53 +0000

    poetry launch

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,12 +3,11 @@ set -e
 
 echo "source .devcontainer/on-shell-start.sh" >> ~/.bashrc
 
-# Setup poetry build env
-poetry completions bash >> ~/.bash_completion
-
 # Setup initial poetry venv (we store it in project so we can add the sb/starbash scripts to the path)
-poetry config virtualenvs.in-project true --local
+# already done and persistent
+# poetry config virtualenvs.in-project true --local
 poetry install
 export PATH="$PWD/.venv/bin:$PATH"
 
-
+# Setup poetry build env
+poetry completions bash >> ~/.bash_completion
