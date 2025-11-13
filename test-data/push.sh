e9725bb530ce6d0e4aea3720e0c00dcdb90e3d9a
Kevin Hester <kevinh@geeksville.com>
2025-11-13 01:41:33 +0000

    test-container wip

--- test-data/push.sh
+++ test-data/push.sh
@@ -6,8 +6,12 @@ set -e
 source .env
 docker login ghcr.io -u geeksville --password-stdin <<< $GH_TOKEN
 
+echo "Building container..."
+
 # Build the "data container" and tag it
 docker build -t ghcr.io/geeksville/starbash/test-data:latest .
+
+echo "Push to ghcr.io..."
 
 # Push your data to the ghcr
 docker push ghcr.io/geeksville/starbash/test-data:latest
